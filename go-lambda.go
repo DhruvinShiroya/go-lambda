c82331efe2bf66f24eacb33c4b5dac98823bf8fe
Dhruvin Shiroya <shiroyadhruvinm@gmail.com>
2024-06-12 19:07:07 -0400

    add api gateway  to aws - provide cors(Cross-Origin Resource Sharing) options like 1. allowed headers 2. allowed methods 3. allowed origin (who can access it , setup for custom use case like frontend client on different network)
    - enable deploy options with logging at method level for debugging.
    - select regional end for deploying
    -  CloudWatchRole is optional if you have configured custom API gateway.
    - create new integration for original lambda myFunction
    - define routes for the api
    - registerRoute will register with struct RegisterUser.
    - registerRoute will accept POST method
    - login route will accept Register user , On method Post
    - protected route will have authorization token with bearer and will access secure end point.

--- go-lambda.go
+++ go-lambda.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/aws/aws-cdk-go/awscdk/v2"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awsapigateway"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awsdynamodb"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awslambda"
 	"github.com/aws/constructs-go/constructs/v10"
@@ -19,24 +20,50 @@ func NewGoLambdaStack(scope constructs.Construct, id string, props *GoLambdaStac
 	}
 	stack := awscdk.NewStack(scope, &id, &sprops)
 
-  myfunction := awslambda.NewFunction(stack,jsii.String("newUsernameValidate"), &awslambda.FunctionProps{
-    Runtime: awslambda.Runtime_PROVIDED_AL2023(),
-    Code: awslambda.AssetCode_FromAsset(jsii.String("lambda/function.zip"), nil),
-    Handler: jsii.String("main") ,
-  })
+	myfunction := awslambda.NewFunction(stack, jsii.String("newUsernameValidate"), &awslambda.FunctionProps{
+		Runtime: awslambda.Runtime_PROVIDED_AL2023(),
+		Code:    awslambda.AssetCode_FromAsset(jsii.String("lambda/function.zip"), nil),
+		Handler: jsii.String("main"),
+	})
 
+	table := awsdynamodb.NewTable(stack, jsii.String("myUserTable"), &awsdynamodb.TableProps{
+		PartitionKey: &awsdynamodb.Attribute{
+			Name: jsii.String("username"),
+			Type: awsdynamodb.AttributeType_STRING,
+		},
+		TableName: jsii.String("userTable"),
+	})
 
-  table := awsdynamodb.NewTable(stack, jsii.String("myUserTable"), &awsdynamodb.TableProps{
-    PartitionKey: &awsdynamodb.Attribute{
-      Name: jsii.String("username"),
-      Type: awsdynamodb.AttributeType_STRING,
-    },
-    TableName: jsii.String("userTable"),
-    
-  })
+	table.GrantReadWriteData(myfunction)
 
-  table.GrantReadWriteData(myfunction)
+	api := awsapigateway.NewRestApi(stack, jsii.String("exampleAPI"), &awsapigateway.RestApiProps{
+		DefaultCorsPreflightOptions: &awsapigateway.CorsOptions{
+			AllowHeaders: jsii.Strings("Content-Type", "X-Amz-Date", "Authorization", "X-Api-Key"),
+			AllowMethods: jsii.Strings("GET", "POST", "PUT", "DELETE", "OPTIONS"),
+			AllowOrigins: jsii.Strings("*"),
+		},
+		DeployOptions: &awsapigateway.StageOptions{
+			LoggingLevel:     awsapigateway.MethodLoggingLevel_INFO,
+			DataTraceEnabled: jsii.Bool(true),
+		},
+		EndpointConfiguration: &awsapigateway.EndpointConfiguration{
+			Types: &[]awsapigateway.EndpointType{awsapigateway.EndpointType_REGIONAL},
+		},
+		CloudWatchRole: jsii.Bool(true),
+	})
 
+	integration := awsapigateway.NewLambdaIntegration(myfunction, nil)
+
+	// define routes
+	registerRoute := api.Root().AddResource(jsii.String("register"), nil)
+	registerRoute.AddMethod(jsii.String("POST"), integration, nil)
+
+	loginRoute := api.Root().AddResource(jsii.String("login"), nil)
+	loginRoute.AddMethod(jsii.String("POST"), integration, nil)
+
+	//protected route
+	protectedRoute := api.Root().AddResource(jsii.String("protected"), nil)
+	protectedRoute.AddMethod(jsii.String("GET"), integration, nil)
 	// The code that defines your stack goes here
 
 	// example resource
