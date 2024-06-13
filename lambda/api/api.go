839b3a78c07db80b2a8477e7f234709edc8ed808
Dhruvin Shiroya <shiroyadhruvinm@gmail.com>
2024-06-12 20:25:13 -0400

    RegisterUserHandler - Add better error handling for registerUser handler - Get the username and password create a new User with username and hashed password - Insert user into database
    LoginUser
    - create LoginRequest struct
    - unmarshal the request body to create LoginRequest
    - get  the user from db
    - use validate password
    - create new jwt token and attach it to the response.

--- lambda/api/api.go
+++ lambda/api/api.go
@@ -1,40 +1,121 @@
 package api
 
 import (
+	"encoding/json"
 	"fmt"
 	"lambda-func/database"
 	"lambda-func/types"
+	"net/http"
+
+	"github.com/aws/aws-lambda-go/events"
 )
 
 type ApiHandler struct {
-	dbStore database.DynamoDBClient
+	dbStore database.UserStore
 }
 
-func NewApiHandler(dbStore database.DynamoDBClient) ApiHandler {
+func NewApiHandler(dbStore database.UserStore) ApiHandler {
 	return ApiHandler{
 		dbStore: dbStore,
 	}
 }
 
-func (api ApiHandler) RegisterUserHandler(event types.RegisterUser) error {
-	if event.Username == "" || event.Password == "" {
-		return fmt.Errorf("request has empty paramters")
+func (api ApiHandler) RegisterUserHandler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+
+	var registerUser types.RegisterUser
+
+	err := json.Unmarshal([]byte(request.Body), &registerUser)
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			Body:       "Invalid request",
+			StatusCode: http.StatusBadRequest,
+		}, err
+	}
+
+	if registerUser.Username == "" || registerUser.Password == "" {
+		return events.APIGatewayProxyResponse{
+			Body:       "Invalid Request - empty field provided",
+			StatusCode: http.StatusBadRequest,
+		}, fmt.Errorf("request has empty paramters")
 	}
 	// does user exist for the Username
-	userExist, err := api.dbStore.DoesUserExist(event.Username)
+	userExist, err := api.dbStore.DoesUserExist(registerUser.Username)
 	if err != nil {
-		return fmt.Errorf("there was an error checking if the user exist %w", err)
+		return events.APIGatewayProxyResponse{
+			Body:       "Internal server error",
+			StatusCode: http.StatusInternalServerError,
+		}, fmt.Errorf("there was an error checking if the user exist %w", err)
 	}
 
 	if userExist {
-		return fmt.Errorf("user with this username exist")
+		return events.APIGatewayProxyResponse{
+			Body:       "User already exist",
+			StatusCode: http.StatusConflict,
+		}, nil
+	}
+
+	user, err := types.NewUser(registerUser)
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			Body:       "internal server error",
+			StatusCode: http.StatusInternalServerError,
+		}, fmt.Errorf("there was an error converting user %w", err)
+
 	}
 
 	// insert the user since it doesn't exist
-	err = api.dbStore.InsertUser(event)
+	err = api.dbStore.InsertUser(user)
 	if err != nil {
-		return fmt.Errorf("there was an error inserting the user %w", err)
+		return events.APIGatewayProxyResponse{
+			Body:       "internal server error",
+			StatusCode: http.StatusInternalServerError,
+		}, fmt.Errorf("there was an error inserting the user %w", err)
 	}
 
-	return nil
+	return events.APIGatewayProxyResponse{
+		Body:       "Successfully registered user",
+		StatusCode: http.StatusOK,
+	}, nil
+}
+
+func (api ApiHandler) LoginUser(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	type LoginRequest struct {
+		Username string `json:"username"`
+		Password string `json:"password"`
+	}
+
+	var loginRequest LoginRequest
+
+	err := json.Unmarshal([]byte(request.Body), &loginRequest)
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			Body:       "Invalid request",
+			StatusCode: http.StatusBadRequest,
+		}, err
+	}
+
+	user, err := api.dbStore.GetUser(loginRequest.Username)
+
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			Body:       "Internal server error",
+			StatusCode: http.StatusBadRequest,
+		}, err
+	}
+
+	if !types.ValidatePassword(user.PasswordHash, loginRequest.Password) {
+		return events.APIGatewayProxyResponse{
+			Body:       "invalid user credential",
+			StatusCode: http.StatusBadRequest,
+		}, nil
+	}
+
+	accessToken := types.CreateToken(user)
+	successMsg := fmt.Sprintf(`{"accessToken" : "%s"}`, accessToken)
+
+	return events.APIGatewayProxyResponse{
+		Body:       successMsg,
+		StatusCode: http.StatusOK,
+	}, nil
+
 }
