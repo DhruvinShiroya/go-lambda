0348a5f11376115afcec3b77b618c56d98f29dd3
Dhruvin Shiroya <shiroyadhruvinm@gmail.com>
2024-06-12 20:36:53 -0400

    create protected handler to handler the request with jwt-token , run this function if the middleware return the request to the function.
    to user the apiRouter in aws-cdk we will user switch case to hit different end points in the api.
    
    - route "/register" will register the user.
    - route "/login" will login the user and return jwt token.
    - route "/protected" will access the protect route with jwt token in auth-header.

--- lambda/main.go
+++ lambda/main.go
@@ -3,8 +3,13 @@ package main
 import (
 	"fmt"
 	"lambda-func/app"
+	"lambda-func/middleware"
+	"log"
+	"net/http"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/joho/godotenv"
 )
 
 type MyEvent struct {
@@ -18,7 +23,33 @@ func HandleRequest(event MyEvent) (string, error) {
 	return fmt.Sprintf("Successfully called by - %s", event.Username), nil
 }
 
+func ProtectedHandler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	return events.APIGatewayProxyResponse{
+		Body:       "This is protected routed",
+		StatusCode: http.StatusOK,
+	}, nil
+}
+
 func main() {
 	myApp := app.NewApp()
-	lambda.Start(myApp.ApiHandler.RegisterUserHandler)
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	lambda.Start(func(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+		switch request.Path {
+		case "/register":
+			return myApp.ApiHandler.RegisterUserHandler(request)
+		case "/login":
+			return myApp.ApiHandler.LoginUser(request)
+		case "/protected":
+			return middleware.ValidateJWTMiddleware(ProtectedHandler)(request)
+		default:
+			return events.APIGatewayProxyResponse{
+				Body:       "Not found",
+				StatusCode: http.StatusNotFound,
+			}, nil
+		}
+	})
 }
